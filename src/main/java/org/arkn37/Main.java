970413e17780c0eea575c077f23acd7dff9ba9be
arkn37 <arkn37.dev@outlook.com>
2025-09-19 22:29:30 -0600

    feat: add json file reader

--- src/main/java/org/arkn37/Main.java
+++ src/main/java/org/arkn37/Main.java
@@ -1,18 +1,23 @@
 package org.arkn37;
 
+import com.google.gson.JsonObject;
+import org.arkn37.service.JsonHandler;
+
+import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to JSON to CSV convertor");
+        System.out.println("-- Welcome to JSON to CSV convertor --");
         int action;
         do {
             System.out.println("""
                     ------------ Menu ------------
-                    1) Read Json File content
-                    9) Exit""");
+                    1) Read Json File content.
+                    9) Exit.""");
             System.out.print("Select an option: ");
 
             while (!scanner.hasNextInt()) {
@@ -22,16 +27,22 @@ public class Main {
 
             action = scanner.nextInt();
             switch (action) {
-                case 1 -> System.out.println("---> Read Json File content\n");
-                case 9 -> System.out.println("""
-                    --------------------------------
-                      Thanks for your visit
-                      See you soon ;)
-                    """);
-                default -> {
-                    System.out.println("Number " + action + " is not a valid option");
-                    System.out.println("Try again");
+                case 1 -> {
+                    System.out.println("--------------------------------");
+                    Optional<List<JsonObject>> jsonObject = JsonHandler.getJsonObject();
+                    jsonObject.ifPresent(jsonObjects ->
+                            System.out.printf("""
+                                    JsonParse:
+                                        %s
+                                    """, jsonObjects)
+                    );
                 }
+                case 9 -> System.out.println("""
+                        --------------------------------
+                          Thanks for your visit
+                          See you soon ;)
+                        """);
+                default -> System.out.println("Number " + action + " is not a valid option. Try again.");
             }
 
         } while (action != 9);
---
+++ src/main/java/org/arkn37/service/JsonHandler.java
@@ -0,0 +1,71 @@
+package org.arkn37.service;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class JsonHandler {
+
+    private JsonHandler() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    private static String readFile(String filePath) throws IOException {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (BufferedReader jsonFile = new BufferedReader(new FileReader(filePath))) {
+            if (!filePath.endsWith(".json") && !filePath.endsWith(".JSON"))
+                throw new IOException("File selected is not a JSON file.");
+
+            String line;
+            while ((line = jsonFile.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+        }
+
+        String stringFile = stringBuilder.toString();
+        if (stringFile.charAt(0) == '{') stringFile = "[" + stringFile + "]";
+
+        return stringFile;
+    }
+
+    private static List<JsonObject> toListObject(String jsonString) throws JsonSyntaxException {
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<JsonObject>>() {
+        }.getType();
+        return gson.fromJson(jsonString, listType);
+    }
+
+    public static Optional<List<JsonObject>> getJsonObject() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Provide file path: ");
+        String filePath = scanner.nextLine();
+        if (filePath.isEmpty()) {
+            System.out.println("File path cannot be empty. Try again.");
+            return Optional.empty();
+        }
+        scanner.close();
+
+        try {
+            String jsonString = readFile(filePath);
+            return Optional.of(toListObject(jsonString));
+        } catch (JsonSyntaxException e) {
+            System.out.println("Error parsing JSON: " + e.getMessage());
+            return Optional.empty();
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+            return Optional.empty();
+        }
+    }
+
+}
